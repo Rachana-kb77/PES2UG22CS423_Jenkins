f2b2a4bea44f0fcbdcf0519cf7cf4e62fa0bb0fa
Rachana-kb77 <159272800+Rachana-kb77@users.noreply.github.com>
2025-03-14 19:56:23 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I am PES2UG22CS423, I have successfully built and run this  \n";
+  cout << "I am rachanaKB, I have successfully built and run this  \n";
   return 0;
 }
